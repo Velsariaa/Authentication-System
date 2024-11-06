40cec5723689ed6f5229adaa48e9a5841fad6f60
keni <kennyranes2319@gmail.com>
2024-11-06 16:45:06 +0800

    contextProvider
    contextProvider

--- ASbackend/laravel_react_authentication/src/main.jsx
+++ ASbackend/laravel_react_authentication/src/main.jsx
@@ -4,9 +4,12 @@ import { RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
 import router from './router.jsx';
+import { ContextProvider } from './contexts/contextprovider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ContextProvider>
     <RouterProvider router={router}/>
+    </ContextProvider>
   </React.StrictMode>,
 )
